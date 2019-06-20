446803a8deb17542864893b2679b0b700f90bf61
Martin <568867383@qq.com>
2019-06-20 18:27:31 +0800

    Update install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-git clone https://github.com/anlory/linux_config.git  ~/linux_config
-cp ~/linux_config/vim/vimrc ~/.vimrc
+git clone https://github.com/anlory/linux_config.git  
+cp linux_config/vim/vimrc ~/.vimrc
 source ~/.vimrc
